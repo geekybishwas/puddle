e76bcdd9ec16418f21296838c8a17acf12201c05
geekybishwas <techiebishwas@gmail.com>
2024-06-23 20:45:56 +0545

    Feat:DashBoard app addeed

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
+    # return render(request,'dashboard/index.html')
     items=Item.objects.filter(created_by=request.user)
 
     return render(request,'dashboard/index.html',{
